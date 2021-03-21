0e1e9ea982bad5c2b851a4352be5fc8ca3da15d3
im6 <ee2win@gmail.com>
2021-03-21 13:54:17 -0400

    clear

--- server/main/route.js
+++
@@ -1,9 +0,0 @@
-var express = require('express'),
-    router = express.Router(),
-    ctr = require("./controller");
-
-
-router.get('/home',ctr.main);
-router.get('/getPoint1',ctr.getPoint1);
-
-module.exports = router;

5f089a6da9fbd898a213f2cc24fcba7ed0595946
im6 <ee2win@gmail.com>
2021-03-22 00:43:54 -0400

    add oss options and upload example

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 	"github.com/spf13/viper"
@@ -43,5 +44,22 @@ func main() {
 	}
 	for _, bucket := range lsRes.Buckets {
 		fmt.Println("Buckets:", bucket.Name)
+	}
+	bucket, err := client.Bucket(configuration.BUCKET)
+	options := []oss.Option{
+		oss.ContentEncoding("gzip"),
+		oss.CacheControl("public, max-age=31104000"),
+	}
+	err = bucket.PutObjectFromFile("temp/howler1.svg", "raw/howler.svg", options...)
+	if err != nil {
+		panic(err)
+	}
+
+	files, err := ioutil.ReadDir("./raw")
+	if err != nil {
+		panic(err)
+	}
+	for _, f := range files {
+		fmt.Println(f.Name())
 	}
 }

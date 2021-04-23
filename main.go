cd302862e37ddbb70c76ffe9c05ed579fcc0fb93
im6 <ee2win@gmail.com>
2021-04-23 00:57:22 -0400

    add gzip action

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"compress/gzip"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"os"
 
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 	"github.com/spf13/viper"
@@ -16,6 +19,10 @@ type Configurations struct {
 }
 
 var configuration Configurations
+
+const iconOssDestination string = "1/icons"
+const rawFilePath string = "./raw"
+const compressedFilePath string = "./compressed"
 
 func loadConfiguration() {
 	viper.SetConfigName("oss-upload.env")
@@ -47,19 +54,48 @@ func main() {
 	}
 	bucket, err := client.Bucket(configuration.BUCKET)
 	options := []oss.Option{
+		oss.ContentType("image/svg+xml"),
 		oss.ContentEncoding("gzip"),
 		oss.CacheControl("public, max-age=31104000"),
 	}
-	err = bucket.PutObjectFromFile("temp/howler1.svg", "raw/howler.svg", options...)
-	if err != nil {
-		panic(err)
-	}
 
-	files, err := ioutil.ReadDir("./raw")
+	files, err := ioutil.ReadDir(rawFilePath)
 	if err != nil {
 		panic(err)
 	}
 	for _, f := range files {
-		fmt.Println(f.Name())
+		var fileName = f.Name()
+		var destPath = fmt.Sprintf("%s/%s.gz", compressedFilePath, fileName)
+
+		srcFile, err := os.Open(fmt.Sprintf("%s/%s", rawFilePath, fileName))
+		if err != nil {
+			panic(err)
+		}
+		defer srcFile.Close()
+
+		dstFile, err := os.Create(destPath)
+		if err != nil {
+			panic(err)
+		}
+		defer dstFile.Close()
+
+		zw := gzip.NewWriter(dstFile)
+		zw.Name = fileName
+		_, err = io.Copy(zw, srcFile)
+		if err != nil {
+			panic(err)
+		}
+
+		zw.Flush()
+		if err := zw.Close(); err != nil {
+			panic(err)
+		}
+
+		err = bucket.PutObjectFromFile(fmt.Sprintf("%s/%s", iconOssDestination, fileName), destPath, options...)
+		if err != nil {
+			panic(err)
+		}
 	}
+
+	fmt.Println("Sent successfully.")
 }
